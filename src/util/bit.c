39e5dfd5700ac2d79856796d718d5afbd7d165ee
Grayson Kippes <gkippes1@gmail.com>
2023-12-21 20:22:37 -0600

    Added bit.c

---
+++ src/util/bit.c
@@ -0,0 +1,17 @@
+#include "bit.h"
+
+bool is_bit_on(uint64_t x, uint64_t bit_pos) {
+	return !!(x & (1LL << bit_pos));
+}
+
+bool is_bit_off(uint64_t x, uint64_t bit_pos) {
+	return !(x & (1LL << bit_pos));
+}
+
+void set_bit_on(uint64_t x, uint64_t bit_pos) {
+	x |= (1LL << bit_pos);
+}
+
+void set_bit_off(uint64_t x, uint64_t bit_pos) {
+	x &= ~(1LL << bit_pos);
+}
--- src/util/bit.h
+++ src/util/bit.h
@@ -1,8 +1,25 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #define IS_BIT_ON(x, pos) ((pos >> x) & 1)
 
 #define TEST_MASK(x, mask) ((x & mask) >= 1)
+
+// Returns true if the bit at the specified position in `x` is one;
+// 	returns false otherwise.
+bool is_bit_on(uint64_t x, uint64_t bit_pos);
+
+// Returns true if the bit at the specified position in `x` is zero;
+// 	returns false otherwise.
+bool is_bit_off(uint64_t x, uint64_t bit_pos);
+
+// Sets the bit at the specified position in `x` to one.
+void set_bit_on(uint64_t x, uint64_t bit_pos);
+
+// Sets the bit at the specified position in `x` to zero.
+void set_bit_off(uint64_t x, uint64_t bit_pos);
 
 #endif	// UTIL_H
